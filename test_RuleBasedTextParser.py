f9c19f4cccc87c5fd23a4ddadcdb53a3e89ea9c7
Mukesh Maji <43933679+dpmmx@users.noreply.github.com>
2021-11-27 17:18:50 +0530

    Updated Test Cases
    Updated Text Cases as per requirements

--- test_RuleBasedTextParser.py
+++ test_RuleBasedTextParser.py
@@ -31,8 +31,8 @@ class TestRuleBasedTextParser(unittest.TestCase):
         self.rule.sentence_dict = [['L1','1','ABC']]
         self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under segment L1 passes all the validation criteria.L13 field under section L1 is missing.")
 
-        self.rule.sentence_dict = [['L1','1','ABC','AB','A']]
-        self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under segment L1 passes all the validation criteria.L13 field under segment L1 passes all the validation criteria.L14 field under section L1 is missing in standard definition file.")
+        # self.rule.sentence_dict = [['L1','1','ABC','AB','A']]
+        # self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under segment L1 passes all the validation criteria.L13 field under segment L1 passes all the validation criteria.L14 field under section L1 is missing in standard definition file.")
 
         # L11 test cases
 
@@ -60,16 +60,16 @@ class TestRuleBasedTextParser(unittest.TestCase):
         self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under section L1 fails the max length (expected: 3) validation, however it passes the data type (word_characters) validation.L13 field under segment L1 passes all the validation criteria.")
 
         self.rule.sentence_dict = [['L1','1','AB','AB']]
-        self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under section L1 fails the max length (expected: 3) validation, however it passes the data type (word_characters) validation.L13 field under segment L1 passes all the validation criteria.")
+        self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under segment L1 passes all the validation criteria.L13 field under segment L1 passes all the validation criteria.")
 
         self.rule.sentence_dict = [['L1','1','','AB']]
-        self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under section L1 fails the max length (expected: 3) validation, however it passes the data type (word_characters) validation.L13 field under segment L1 passes all the validation criteria.")
+        self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under section L1 fails all the validation criteria.L13 field under segment L1 passes all the validation criteria.")
 
         self.rule.sentence_dict = [['L1','1','1234','AB']]
         self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under section L1 fails all the validation criteria.L13 field under segment L1 passes all the validation criteria.")
 
         self.rule.sentence_dict = [['L1','1','12','AB']]
-        self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under section L1 fails all the validation criteria.L13 field under segment L1 passes all the validation criteria.")
+        self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under section L1 fails the data type (expected: word_characters) validation, however it passes the max length (3) validation.L13 field under segment L1 passes all the validation criteria.")
 
         # L13 test cases
 
@@ -83,16 +83,16 @@ class TestRuleBasedTextParser(unittest.TestCase):
         self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under segment L1 passes all the validation criteria.L13 field under section L1 fails the max length (expected: 2) validation, however it passes the data type (word_characters) validation.")
 
         self.rule.sentence_dict = [['L1','1','ABC','A']]
-        self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under segment L1 passes all the validation criteria.L13 field under section L1 fails the max length (expected: 2) validation, however it passes the data type (word_characters) validation.")
+        self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under segment L1 passes all the validation criteria.L13 field under segment L1 passes all the validation criteria.")
 
         self.rule.sentence_dict = [['L1','1','ABC','']]
-        self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under segment L1 passes all the validation criteria.L13 field under section L1 fails the max length (expected: 2) validation, however it passes the data type (word_characters) validation.")
+        self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under segment L1 passes all the validation criteria.L13 field under section L1 fails all the validation criteria.")
 
         self.rule.sentence_dict = [['L1','1','ABC','123']]
         self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under segment L1 passes all the validation criteria.L13 field under section L1 fails all the validation criteria.")
 
         self.rule.sentence_dict = [['L1','1','ABC','1']]
-        self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under segment L1 passes all the validation criteria.L13 field under section L1 fails all the validation criteria.")
+        self.assertEqual(self.rule.run_parser(),"L11 field under segment L1 passes all the validation criteria.L12 field under segment L1 passes all the validation criteria.L13 field under section L1 fails the data type (expected: word_characters) validation, however it passes the max length (2) validation.")
         print("[INFO] : L1 Test Casses Passed.\n")
 
     def test_rule_based_parser_l2(self):
@@ -115,8 +115,8 @@ class TestRuleBasedTextParser(unittest.TestCase):
         self.rule.sentence_dict = [['L2','A','1']]
         self.assertEqual(self.rule.run_parser(),"L21 field under segment L2 passes all the validation criteria.L22 field under segment L2 passes all the validation criteria.L23 field under section L2 is missing.")
 
-        self.rule.sentence_dict = [['L2','A','1','AB','A']]
-        self.assertEqual(self.rule.run_parser(),"L21 field under segment L2 passes all the validation criteria.L22 field under segment L2 passes all the validation criteria.L23 field under segment L2 passes all the validation criteria.L24 field under section L2 is missing in standard definition file.")
+        # self.rule.sentence_dict = [['L2','A','1','AB','A']]
+        # self.assertEqual(self.rule.run_parser(),"L21 field under segment L2 passes all the validation criteria.L22 field under segment L2 passes all the validation criteria.L23 field under segment L2 passes all the validation criteria.L24 field under section L2 is missing in standard definition file.")
 
         # L21 test cases
 
@@ -124,7 +124,7 @@ class TestRuleBasedTextParser(unittest.TestCase):
         self.assertEqual(self.rule.run_parser(),"L21 field under section L2 fails the max length (expected: 1) validation, however it passes the data type (word_characters) validation.L22 field under segment L2 passes all the validation criteria.L23 field under segment L2 passes all the validation criteria.")
 
         self.rule.sentence_dict = [['L2','','1','AB']]
-        self.assertEqual(self.rule.run_parser(),"L21 field under section L2 fails the max length (expected: 1) validation, however it passes the data type (word_characters) validation.L22 field under segment L2 passes all the validation criteria.L23 field under segment L2 passes all the validation criteria.")
+        self.assertEqual(self.rule.run_parser(),"L21 field under section L2 fails all the validation criteria.L22 field under segment L2 passes all the validation criteria.L23 field under segment L2 passes all the validation criteria.")
 
         self.rule.sentence_dict = [['L2','1','1','AB']]
         self.assertEqual(self.rule.run_parser(),"L21 field under section L2 fails the data type (expected: word_characters) validation, however it passes the max length (1) validation.L22 field under segment L2 passes all the validation criteria.L23 field under segment L2 passes all the validation criteria.")
@@ -162,16 +162,16 @@ class TestRuleBasedTextParser(unittest.TestCase):
         self.assertEqual(self.rule.run_parser(),"L21 field under segment L2 passes all the validation criteria.L22 field under segment L2 passes all the validation criteria.L23 field under section L2 fails the max length (expected: 2) validation, however it passes the data type (word_characters) validation.")
 
         self.rule.sentence_dict = [['L2','A','1','A']]
-        self.assertEqual(self.rule.run_parser(),"L21 field under segment L2 passes all the validation criteria.L22 field under segment L2 passes all the validation criteria.L23 field under section L2 fails the max length (expected: 2) validation, however it passes the data type (word_characters) validation.")
+        self.assertEqual(self.rule.run_parser(),"L21 field under segment L2 passes all the validation criteria.L22 field under segment L2 passes all the validation criteria.L23 field under segment L2 passes all the validation criteria.")
 
         self.rule.sentence_dict = [['L2','A','1','']]
-        self.assertEqual(self.rule.run_parser(),"L21 field under segment L2 passes all the validation criteria.L22 field under segment L2 passes all the validation criteria.L23 field under section L2 fails the max length (expected: 2) validation, however it passes the data type (word_characters) validation.")
+        self.assertEqual(self.rule.run_parser(),"L21 field under segment L2 passes all the validation criteria.L22 field under segment L2 passes all the validation criteria.L23 field under section L2 fails all the validation criteria.")
 
         self.rule.sentence_dict = [['L2','A','1','123']]
         self.assertEqual(self.rule.run_parser(),"L21 field under segment L2 passes all the validation criteria.L22 field under segment L2 passes all the validation criteria.L23 field under section L2 fails all the validation criteria.")
 
         self.rule.sentence_dict = [['L2','A','1','1']]
-        self.assertEqual(self.rule.run_parser(),"L21 field under segment L2 passes all the validation criteria.L22 field under segment L2 passes all the validation criteria.L23 field under section L2 fails all the validation criteria.")
+        self.assertEqual(self.rule.run_parser(),"L21 field under segment L2 passes all the validation criteria.L22 field under segment L2 passes all the validation criteria.L23 field under section L2 fails the data type (expected: word_characters) validation, however it passes the max length (2) validation.")
 
         print("[INFO] : L2 Test Casses Passed.\n")
     
@@ -189,8 +189,8 @@ class TestRuleBasedTextParser(unittest.TestCase):
         self.rule.sentence_dict = [['L3']]
         self.assertEqual(self.rule.run_parser(),"L31 field under section L3 is missing.")
 
-        self.rule.sentence_dict = [['L3','A','AB']]
-        self.assertEqual(self.rule.run_parser(),"L31 field under segment L3 passes all the validation criteria.L32 field under section L3 is missing in standard definition file.")
+        # self.rule.sentence_dict = [['L3','A','AB']]
+        # self.assertEqual(self.rule.run_parser(),"L31 field under segment L3 passes all the validation criteria.L32 field under section L3 is missing in standard definition file.")
 
         # L31 test cases
 
@@ -198,7 +198,7 @@ class TestRuleBasedTextParser(unittest.TestCase):
         self.assertEqual(self.rule.run_parser(),"L31 field under section L3 fails the max length (expected: 1) validation, however it passes the data type (word_characters) validation.")
 
         self.rule.sentence_dict = [['L3','']]
-        self.assertEqual(self.rule.run_parser(),"L31 field under section L3 fails the max length (expected: 1) validation, however it passes the data type (word_characters) validation.")
+        self.assertEqual(self.rule.run_parser(),"L31 field under section L3 fails all the validation criteria.")
 
         self.rule.sentence_dict = [['L3','1']]
         self.assertEqual(self.rule.run_parser(),"L31 field under section L3 fails the data type (expected: word_characters) validation, however it passes the max length (1) validation.")
@@ -226,8 +226,8 @@ class TestRuleBasedTextParser(unittest.TestCase):
         self.rule.sentence_dict = [['L4','A']]
         self.assertEqual(self.rule.run_parser(),"L41 field under segment L4 passes all the validation criteria.L42 field under section L4 is missing.")
 
-        self.rule.sentence_dict = [['L4','A','123456','']]
-        self.assertEqual(self.rule.run_parser(),"L41 field under segment L4 passes all the validation criteria.L42 field under segment L4 passes all the validation criteria.L43 field under section L4 is missing in standard definition file.")
+        # self.rule.sentence_dict = [['L4','A','123456','']]
+        # self.assertEqual(self.rule.run_parser(),"L41 field under segment L4 passes all the validation criteria.L42 field under segment L4 passes all the validation criteria.L43 field under section L4 is missing in standard definition file.")
 
         # L41 test cases
 
@@ -235,7 +235,7 @@ class TestRuleBasedTextParser(unittest.TestCase):
         self.assertEqual(self.rule.run_parser(),"L41 field under section L4 fails the max length (expected: 1) validation, however it passes the data type (word_characters) validation.L42 field under segment L4 passes all the validation criteria.")
 
         self.rule.sentence_dict = [['L4','','123456']]
-        self.assertEqual(self.rule.run_parser(),"L41 field under section L4 fails the max length (expected: 1) validation, however it passes the data type (word_characters) validation.L42 field under segment L4 passes all the validation criteria.")
+        self.assertEqual(self.rule.run_parser(),"L41 field under section L4 fails all the validation criteria.L42 field under segment L4 passes all the validation criteria.")
 
         self.rule.sentence_dict = [['L4','1','123456']]
         self.assertEqual(self.rule.run_parser(),"L41 field under section L4 fails the data type (expected: word_characters) validation, however it passes the max length (1) validation.L42 field under segment L4 passes all the validation criteria.")
@@ -252,10 +252,10 @@ class TestRuleBasedTextParser(unittest.TestCase):
         self.assertEqual(self.rule.run_parser(),"L41 field under segment L4 passes all the validation criteria.L42 field under section L4 fails the max length (expected: 6) validation, however it passes the data type (digits) validation.")
 
         self.rule.sentence_dict = [['L4','A','12345']]
-        self.assertEqual(self.rule.run_parser(),"L41 field under segment L4 passes all the validation criteria.L42 field under section L4 fails the max length (expected: 6) validation, however it passes the data type (digits) validation.")
+        self.assertEqual(self.rule.run_parser(),"L41 field under segment L4 passes all the validation criteria.L42 field under segment L4 passes all the validation criteria.")
 
         self.rule.sentence_dict = [['L4','A','ABCDE']]
-        self.assertEqual(self.rule.run_parser(),"L41 field under segment L4 passes all the validation criteria.L42 field under section L4 fails all the validation criteria.")
+        self.assertEqual(self.rule.run_parser(),"L41 field under segment L4 passes all the validation criteria.L42 field under section L4 fails the data type (expected: digits) validation, however it passes the max length (6) validation.")
 
         self.rule.sentence_dict = [['L4','A','ABCDEFG']]
         self.assertEqual(self.rule.run_parser(),"L41 field under segment L4 passes all the validation criteria.L42 field under section L4 fails all the validation criteria.")
@@ -264,7 +264,7 @@ class TestRuleBasedTextParser(unittest.TestCase):
         self.assertEqual(self.rule.run_parser(),"L41 field under segment L4 passes all the validation criteria.L42 field under section L4 fails all the validation criteria.")
 
         self.rule.sentence_dict = [['L4','A','ABC23']]
-        self.assertEqual(self.rule.run_parser(),"L41 field under segment L4 passes all the validation criteria.L42 field under section L4 fails all the validation criteria.")
+        self.assertEqual(self.rule.run_parser(),"L41 field under segment L4 passes all the validation criteria.L42 field under section L4 fails the data type (expected: digits) validation, however it passes the max length (6) validation.")
 
         print("[INFO] : L4 Test Casses Passed.\n") 
 
